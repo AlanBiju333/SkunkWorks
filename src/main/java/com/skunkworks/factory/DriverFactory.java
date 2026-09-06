84d2f1b150bdb5b088ebb5f39240ca237aa1bfe2
Alan Biju <alankulathumkal333@gmail.com>
2026-09-06 13:43:17 +0530

    updated DriverFactory.java and removed WebDriverManager dependency

--- src/main/java/com/skunkworks/factory/DriverFactory.java
+++ src/main/java/com/skunkworks/factory/DriverFactory.java
@@ -1,31 +1,27 @@
 package com.skunkworks.factory;
 
 import com.skunkworks.config.ConfigReader;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();//helps to assign individual browser instances to each test, erasing clashes
+    private static final ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();//helps to assign individual browser instances to each test, erasing clashes
 
     public static void initDriver() {
         String browser = ConfigReader.getProperty("browser");
         switch (browser.toLowerCase()) {
 
             case "chrome":
-                WebDriverManager.chromedriver().setup();
                 tlDriver.set(new ChromeDriver());
                 break;
 
             case "firefox":
-                WebDriverManager.firefoxdriver().setup();
                 tlDriver.set(new FirefoxDriver());
                 break;
 
             case "edge":
-                WebDriverManager.edgedriver().setup();
                 tlDriver.set(new EdgeDriver());
                 break;
 
@@ -39,8 +35,9 @@ public class DriverFactory {
     }
 
     public static void quitDriver() {
-        if (tlDriver.get() != null) {
-            tlDriver.get().quit();
+        WebDriver driver = tlDriver.get();
+        if (driver != null) {
+            driver.quit();
             tlDriver.remove();
         }
     }
